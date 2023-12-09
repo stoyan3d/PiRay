a63a818b33a0972d47f08d3140d46303318d7e48
Stoyan Dimitrov <stoyan3d@gmail.com>
2023-12-09 13:40:19 +0000

    Adding a camera system

--- PiRay/src/Renderer.h
+++ PiRay/src/Renderer.h
@@ -4,6 +4,8 @@
 
 #include "Walnut/Image.h"
 #include "glm/glm.hpp"
+#include "Camera.h"
+#include "Ray.h"
 
 
 class Renderer
@@ -12,10 +14,11 @@ public:
 	Renderer() = default;
 
 	void OnResize(uint32_t width, uint32_t height);
-	void Render(const glm::vec3 &sphereColor);
+	void Render(const glm::vec3 &sphereColor, const Camera& camera);
 
 	std::shared_ptr<Walnut::Image> GetFinalImage() { return m_FinalImage; };
-	glm::vec4 PerPixel(glm::vec2 coord, const glm::vec3 &sphereColor);
+private:
+	glm::vec4 TraceRay(const Ray& ray, const glm::vec3 &sphereColor);
 private:
 	std::shared_ptr<Walnut::Image> m_FinalImage;
 	uint32_t* m_ImageData;
